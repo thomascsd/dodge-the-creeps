3f75a75c088357af2a6f80ea799de7d96ed0ce6f
thomascsd <thomascsd808@gmail.com>
2025-05-25 16:06:25 +0800

    新增遊戲啟動邏輯、計時器管理及 Mob 生成設定，並更新場景配置

--- Main.cs
+++ Main.cs
@@ -7,14 +7,23 @@ public partial class Main : Node
 
     [Export]
     public PackedScene MobScene { get; set; }
+    private Timer _mobTimer;
+    private Timer _scoreTimer;
+    private Timer _startTimer;
+
+    public override void _Ready()
+    {
+        GD.Print("Game Started");
+        this.NewGame();
+    }
 
     public void GameOver()
     {
-        this.GetNode<Timer>("MobTimer").Stop();
-        this.GetNode<Timer>("ScoreTimer").Stop();
+        _mobTimer.Stop();
+        _scoreTimer.Stop();
     }
 
-    public void GameStart()
+    public void NewGame()
     {
         this._Score = 0;
 
@@ -23,17 +32,41 @@ public partial class Main : Node
 
         player.Position = startPosition.Position;
 
-        this.GetNode<Timer>("StartTimer").Start();
+        this._mobTimer = this.GetNode<Timer>("MobTimer");
+        this._mobTimer.Timeout += this.OnMobTimerTimeout;
+
+        this._scoreTimer = this.GetNode<Timer>("ScoreTimer");
+        this._scoreTimer.Timeout += this.OnScoreTimerTimeout;
+
+        this._startTimer = this.GetNode<Timer>("StartTimer");
+        this._startTimer.Timeout += this.OnStartTimerTimeout;
+
+        this._startTimer.Start();
+
+        GD.Print("New Game");
     }
 
     public void OnMobTimerTimeout()
     {
         Mob mob = this.MobScene.Instantiate<Mob>();
+
+        GD.Print("Mob Spawned");
+        GD.Print(mob.ToString());
+
         var mobSpawnPostion = this.GetNode<PathFollow2D>("MobPath/MobSpawnLocation");
 
         mobSpawnPostion.ProgressRatio = GD.Randf();
+        mob.Position = mobSpawnPostion.Position;
 
+        var direction = mobSpawnPostion.Rotation + MathF.PI / 2;
 
+        direction += (float)GD.RandRange(-MathF.PI / 4, MathF.PI / 4);
+        mob.Rotation = direction;
+
+        var velocity = new Vector2((float)GD.RandRange(150, 250), 0);
+        mob.LinearVelocity = velocity.Rotated(direction);
+
+        this.AddChild(mob);
     }
 
     public void OnScoreTimerTimeout()
@@ -43,9 +76,9 @@ public partial class Main : Node
 
     public void OnStartTimerTimeout()
     {
-        this.GetNode<Timer>("MobTimer").Start();
-        this.GetNode<Timer>("ScoreTimer").Start();
-
+        GD.Print("Start Timer Timeout");
+        this._mobTimer.Start();
+        this._scoreTimer.Start();
     }
 
 }

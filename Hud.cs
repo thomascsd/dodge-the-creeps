f4a67f51380af9d3eaa1face6294ce3f81386949
thomascsd <thomascsd808@gmail.com>
2025-05-31 21:27:20 +0800

    新增 HUD 類別的顯示訊息與計時器功能，改善遊戲結束時的訊息顯示邏輯

--- Hud.cs
+++ Hud.cs
@@ -6,4 +6,62 @@ public partial class Hud : CanvasLayer
 
     [Signal]
     public delegate void GameStartEventHandler();
+
+    private Timer _messageTimer;
+
+    private Button _startButton;
+
+    override public void _Ready()
+    {
+        _startButton = this.GetNode<Button>("StartButton");
+        _startButton.Pressed += this.OnStartButtonPressed;
+
+        _messageTimer = this.GetNode<Timer>("MessageTimer");
+        _messageTimer.Timeout += this.OnMessageTimerTimeout;
+    }
+
+    public void ShowMessage(string message)
+    {
+        var lbl = this.GetNode<Label>("Message");
+
+        lbl.Text = message;
+        lbl.Show();
+    }
+
+    public void ShowMessageAndTimerStart(string message)
+    {
+        this.ShowMessage(message);
+        _messageTimer.Start();
+    }
+
+
+    public async void ShowGameOver()
+    {
+        this.ShowMessageAndTimerStart("Game Over");
+
+        await this.ToSignal(_messageTimer, Timer.SignalName.Timeout);
+
+        this.ShowMessage("Dodge the Creeps!");
+
+
+        await this.ToSignal(this.GetTree().CreateTimer(1.0), Timer.SignalName.Timeout);
+
+        _startButton.Show();
+    }
+
+    public void UpdateScore(int score)
+    {
+        this.GetNode<Label>("ScoreLabel").Text = score.ToString();
+    }
+
+    public void OnStartButtonPressed()
+    {
+        _startButton.Hide();
+        this.EmitSignal(SignalName.GameStart);
+    }
+
+    public void OnMessageTimerTimeout()
+    {
+        this.GetNode<Label>("Message").Hide();
+    }
 }
